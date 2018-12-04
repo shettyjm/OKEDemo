361cf4e6e6d17a62103d40c7ae1c39b093096517
shettyjm <jagshetty-oracle@Jags-MacBook-Pro.local>
2018-12-04 15:42:36 -0800

    testing scripts

--- test/mocktest.js
+++ test/mocktest.js
@@ -1,8 +1,10 @@
 var assert = require('assert');
+const sinon = require('sinon');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal([1,2,3].indexOf(4), -1);
+      
     });
   });
 });
